4349cbaa1cf60251fdab3435dd7b5840255085a6
happysalada <raphael.megzari@gmail.com>
2017-07-07 11:39:56 +0200

    do not hash favico and launcher ico

--- public/sw.js
+++ public/sw.js
@@ -19,28 +19,28 @@ importScripts('workbox-sw.prod.v1.0.1.js');
  */
 const fileManifest = [
   {
-    "url": "/assets/favicon-6b2d39.ico",
+    "url": "/assets/favicon.ico",
     "revision": "437d9580a62f42c1ebaa355456174879"
   },
   {
-    "url": "/assets/launcher_icon_2x-a86699.png",
+    "url": "/assets/launcher_icon_2x.png",
     "revision": "b1003bde6748cf0dafedbd8dd4dce38b"
   },
   {
-    "url": "/assets/launcher_icon_3x-6b2d39.png",
+    "url": "/assets/launcher_icon_3x.png",
     "revision": "437d9580a62f42c1ebaa355456174879"
   },
   {
-    "url": "/assets/launcher_icon-ef9adf.png",
+    "url": "/assets/launcher_icon.png",
     "revision": "5e5956232b98810da18c8251d01b1c6e"
   },
   {
-    "url": "/assets/manifest-d90ad4.json",
-    "revision": "232cd97394ef5662317276302cec48ab"
+    "url": "/assets/manifest-c71906.json",
+    "revision": "fffebc5677636ec7c632e66baacf92b8"
   },
   {
     "url": "/index.html",
-    "revision": "ad2bd3e1f8868ba763999650603a02fc"
+    "revision": "7a59683a5eb918b0f3ae0df44651c59c"
   },
   {
     "url": "/js/analytics.js.map",
